79bc6c4282c40ae05dcb1cb12cfa17e9d10533ce
dbritto <dawson.britto@hotmail.com>
2017-08-22 14:03:03 +0800

    Po Query

---
+++ USYD_demo/Forms/PO/POQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace USYD_demo
+{
+    [Serializable]
+    public class POQuery
+    {
+        public  string AccountNumber { get; set; }
+        public  string PONumber { get; set; }
+        public  string UniKey { get; set; }
+
+        
+    }
+}
